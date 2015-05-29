289f3565009acec3f4f5fdc2c7ef81e0f7f7975d
Lwatt <laurencewatts@hotmail.co.uk>
2015-05-29 20:50:46 +0100

    Fix: now requires user to input api key instead of trying to fetch it

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var sessionID;
-
-//function heavily inspired by node-steam-tradeoffers method
-function getAPIKey(){
-	request('https://steamcommunity.com/dev/apikey', function(error, response, body){
-		if(!error && response.statusCode == 200){
-			var $ = cheerio.load(body);
-			if($('#mainContents h2').html() == 'Access Denied'){
-				console.log('access denied getting api key, probably limited account');
-			} else if($('#bodyContents_ex h2').html() == 'Your Steam Web API Key'){
-				var key = $('#bodyContents_ex p').html().split(' ')[1];
-				return key;
-			} else {
-				console.log('Registering Web API key');
-				request.post({
-					uri: 'https://steamcommunity.com/dev/registerkey',
-					agreeToTerms : 'agreed',
-					sessionid: sessionID,
-					submit: 'Register'
-				}, function(error, response, body){
-					return getAPIKey();
-				});
-			}
-		}
-	});
-}
+var apikey = '';
 
 var getUserInfo = function (steamid, callback){
-	var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+getAPIKey()+'&steamids='+ steamid
+	var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+apikey+'&steamids='+ steamid
 	request({
 		url: url,
 		json: true
@@ -41,8 +16,8 @@ var getUserInfo = function (steamid, callback){
 	});
 }
 
-var setup = function(sID, callback){
-	sessionID = sID;
+var setup = function(key, callback){
+	apikey = key;
 }
 
 module.exports = {
